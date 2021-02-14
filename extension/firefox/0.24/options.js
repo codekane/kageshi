f5a1c89b854dbeaa564b5ae22a1fc412c96a9d4a
codekane <ryan.horricks@gmail.com>
2021-02-13 21:38:37 -0700

    Half-baked implentation of font size normalization... Kinda drunk. It owrks as is, but there's no working preferences toggle... It's mostly there, I'm just too drunk to make it work. Another day.

---
+++ extension/firefox/0.24/options.js
@@ -0,0 +1,39 @@
+function styleCheck(event) {
+  let toggle = event.target.checked === true;
+  browser.storage.local.set({
+    dongStyle: toggle
+  });
+}
+
+function fontCheck(event) {
+  let toggle = event.target.checked === true;
+  browser.storage.local.set({
+    normFonts: toggle
+  });
+
+function sizeCheck(event) {
+  let toggle = event.target.checked === true;
+  browser.storage.local.set({
+    normFonts: toggle
+  });
+}
+
+function restoreOptions() {
+  function dongToggle(result) {
+    document.querySelector('#dongStyle').checked = result.dongStyle;
+  }
+  function fontToggle(result) {
+    document.querySelector('#normFonts').checked = result.normFonts;
+  }
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+  let dongGet = browser.storage.local.get('dongStyle');
+  dongGet.then(dongToggle, onError);
+  let fontGet = browser.storage.local.get('normFonts');
+  fontGet.then(fontToggle, onError);
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("#dongStyle").addEventListener("change", styleCheck);
+document.querySelector('#normFonts').addEventListener('change', fontCheck);
